cf5986c66b00b40b36183a5cb9fd72e501ce58c1
Shin-SK <shin@sk-tokyo.net>
2025-08-06 16:05:21 +0900

    0806

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -4,98 +4,16 @@ import { useUser } from '@/stores/useUser'
 
 /* --- レイアウト --- */
 import AdminLayout      from '@/layouts/AdminLayout.vue'
-import DriverLayout     from '@/layouts/DriverLayout.vue'
 import CastLayout       from '@/layouts/CastLayout.vue'
 
-/* --- 画面 --- */
-import ReservationList       from '@/views/ReservationList.vue'
-import ReservationFormAdmin  from '@/views/ReservationFormAdmin.vue'
-import ReservationFormDriver from '@/views/ReservationFormDriver.vue'
-import Login                 from '@/views/Login.vue'
-import TimelineAdmin         from '@/views/TimelineAdmin.vue'
-import DriverMypage        from '@/views/DriverMypage.vue'
-import CustomerList          from '@/views/CustomerList.vue'
-import CustomerForm          from '@/views/CustomerForm.vue'
-import AdminCastForm              from '@/views/AdminCastForm.vue'
-import AdminDriverList              from '@/views/AdminDriverList.vue'
-import AdminDriverForm              from '@/views/AdminDriverForm.vue'
-import UserProfileEdit              from '@/views/UserProfileEdit.vue'
-import Sales           from '@/views/Sales.vue'
-import PLMonthly           from '@/views/PLMonthly.vue'
-import PLDaily  from '@/views/PLDaily.vue'
-import PLYearly  from '@/views/PLYearly.vue'
-import ExpenseForm  from '@/views/ExpenseForm.vue'
-
 const routes = [
-  { path: '/', redirect: '/dashboard' },
-
-  /* ---------- 管理者 ---------- */
-  {
-    path: '/',
-    component: AdminLayout,
-    meta: { requiresAuth: true, adminOnly: true },
-    children: [
-      { path: 'reservations', component: ReservationList, meta: { title: '予約一覧' } },
-      { path: 'reservations/new',     component: ReservationFormAdmin, meta: { title: '新規予約' } },
-      { path: 'reservations/:id',     component: ReservationFormAdmin, name: 'admin-reservation-detail', meta: { title: '予約詳細' } },
-      { path: 'reservations/:id(\\d+)', component: ReservationFormAdmin, meta: { title: '予約詳細' }},
-      { path: 'timeline',             component: TimelineAdmin },
-      { path: 'customers',            component: CustomerList, meta: { title: '顧客情報' }},
-      { path: 'customers/new',        component: CustomerForm, meta: { title: '新規顧客' }},
-      { path: 'customers/:id',        component: CustomerForm, meta: { title: '顧客編集' } },
-      { path: 'casts/new',            component: AdminCastForm, meta: { title: '新規キャスト' } },
-      { path: 'casts/:id',            component: AdminCastForm, meta: { title: 'キャスト編集' } },
-      { path: 'drivers',                component: AdminDriverList, meta: { title: 'ドライバー情報' } },
-      { path: 'drivers/new',            component: AdminDriverForm, meta: { title: '新規ドライバー' } },
-      { path: 'drivers/:id',            component: AdminDriverForm, meta: { title: 'ドライバー編集' } },
-      { path: '/driver-shifts', name: 'driver-shift-list', component: () => import('@/views/DriverShiftList.vue')},
-      {
-        path: '/driver-shifts/driver/:driverId(\\d+)',   // ★NEW
-        name: 'driver-shift-by-driver',
-        component: () => import('@/views/DriverShift.vue'),
-        props: true   // → route.params.driverId で受け取れる
-      },
-      {
-        path: '/driver-shifts/:shiftId(\\d+)',   // id? → shiftId 固定
-        name: 'driver-shift-detail',
-        component: () => import('@/views/DriverShift.vue'),
-        props: true
-      },
-      // { path: 'cast-shifts', component: () => import('@/views/CastShift.vue') },
-      { path: 'sales', component: Sales },
-      /* ---------- PL ---------- */
-      { path: '/pl/monthly', component: PLMonthly },
-      { path: '/pl/daily', component: PLDaily },
-      { path: '/pl/yearly', component: PLYearly },
-      { path: '/expense/form', component: ExpenseForm },
-    ],
-  },
-
-
-  /* ---------- ドライバー ---------- */
-  {
-    path: '/driver',
-    component: DriverLayout,
-    meta: { requiresAuth: true, driverOnly: true },
-    children: [
-      { path: '', redirect: '/driver/mypage' },
-      { path: 'mypage',        component: DriverMypage },
-      { path: 'reservations/:id', component: ReservationFormDriver },
-      { path: 'profile', component: UserProfileEdit }   // /driver/profile など
-
-    ],
-  },
-
-  /* ---------- 認証 ---------- */
-  { path: '/login', component: Login },
-
-
-  /* ---------- キャバクラ編 伝票 ---------- */
+  // ---------- キャバクラ版 ---------- //
   {
   path: '/bills',
     component: AdminLayout,
     meta: { requiresAuth: true, adminOnly: true },
     children: [
+          { path: '/', redirect: '/dashboard' },
           { path: '/dashboard',   component: () => import('@/views/DashboardAdmin.vue'), meta: { title: 'DASHBOARD' }},
           { path: '/dashboard/list',   component: () => import('@/views/DashboardList.vue'), meta: { title: 'DASHBOARD-LIST' }},
           { path: '/dashboard/timeline',   component: () => import('@/views/DashboardGantt.vue'), meta: { title: 'DASHBOARD-TIMELINE' }},
@@ -115,22 +33,20 @@ const routes = [
     ],
   },
 
+  // ---------- キャスト ---------- //
   {
     path: '/cast',
     component: CastLayout,
     meta: { requiresAuth: true }, // ★ 後で castOnly へ
     children: [
       { path: '', redirect: '/cast/mypage' },
-
-      // マイページは「必ず :id を付ける」形に
       { path: 'mypage/:id(\\d+)', component: () => import('@/views/CastMypage.vue'), name: 'cast-mypage',meta: { title: 'MyPage' } },
-
       { path: 'sales', component: () => import('@/views/CastSalesDetail.vue') },
-      { path: 'profile', component: UserProfileEdit },
     ],
   },
 
-
+  // ---------- その他認証系 ---------- //
+ { path: '/login', component: () => import('@/views/Login.vue') },
 
 ]
 
@@ -138,11 +54,6 @@ const router = createRouter({
   history: createWebHistory(),
   routes,
 })
-
-
-
-
-
 
 
 /* ---- ルートガード（ほぼ元のまま／親メタを見るだけ） ---- */
